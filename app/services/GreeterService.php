3620ed714c3ea6560227bed71e6010ca7a00b971
luoxiaojun <luoxiaojun@dodoca.com>
2019-05-12 20:26:17 +0800

    grpc

--- app/services/GreeterService.php
+++ app/services/GreeterService.php
@@ -4,9 +4,9 @@ namespace App\services;
 
 use Helloworld\HelloReply;
 use Helloworld\HelloRequest;
-use SwFwLess\services\BaseService;
+use SwFwLess\services\GrpcUnaryService;
 
-class GreeterService extends BaseService
+class GreeterService extends GrpcUnaryService
 {
     public function sayHello(HelloRequest $helloRequest) : HelloReply
     {
