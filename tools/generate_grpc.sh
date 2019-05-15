806b7616d3df08099c7ad2f1fe9583e83c76985b
luoxiaojun <luoxiaojun@dodoca.com>
2019-05-15 19:27:03 +0800

    grpc

--- tools/generate_grpc.sh
+++ tools/generate_grpc.sh
@@ -3,13 +3,13 @@
 current=$(pwd)
 
 ../vendor/bin/generator \
---proto_path=$current/../app/grpc/protos \
---php_out=$current/../app/grpc \
---grpc_out=$current/../app/grpc \
+--proto_path=$current/../app/grpc-gen/protos \
+--php_out=$current/../app/grpc-gen \
+--grpc_out=$current/../app/grpc-gen \
 --grpc_php_out=$current/../app/services \
 --plugin=protoc-gen-grpc=$current/$1 \
 --custom_plugin=protoc-gen-grpc-php=$current/../vendor/bin/protoc-gen-grpc-php-plugin.php \
-$current/../app/grpc/protos
+$current/../app/grpc-gen/protos
 
 cd $current/../ && composer dump-autoload
 cd $current
