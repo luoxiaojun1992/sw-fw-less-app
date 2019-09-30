a593b1dc2f1876024927f9961d7f5f20f91bda28
luoxiaojun <luoxiaojun@dodoca.com>
2019-09-30 17:24:29 +0800

    add apollo config

---
+++ config/apollo.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'config_server' => env('APOLLO_CONFIG_SERVER', 'http://127.0.0.1:8080'),
+    'app_id' => env('APOLLO_APP_ID', 'sw-fw-less'),
+    'cluster' => env('APOLLO_CLUSTER', 'default'),
+    'client_ip' => env('APOLLO_CLIENT_IP', '127.0.0.1'),
+    'pull_timeout' => envDouble('APOLLO_PULL_TIMEOUT', 10),
+    'namespace' => env('APOLLO_NAMESPACE', 'application'),
+    'release_key' => env('APOLLO_RELEASE_KEY', ''),
+];
