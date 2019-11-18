f74ebc233bdb7e77284db49e4650813e93039067
luoxiaojun <luoxiaojun@dodoca.com>
2019-11-18 13:49:46 +0800

    optimizen

--- config/server.php
+++ config/server.php
@@ -12,6 +12,9 @@ $serverConfig = [
     'open_http2_protocol' => envBool('SERVER_OPEN_HTTP2', false),
     'task_worker_num' => envInt('SERVER_TASK_WORKER_NUM', 0),
     'task_enable_coroutine' => envBool('SERVER_TASK_ENABLE_COROUTINE', false),
+    'open_tcp_nodelay' => envBool('SERVER_OPEN_TCP_NODELAY', true),
+    'tcp_fastopen' => envBool('SERVER_TCP_FASTOPEN', true),
+    'max_coroutine' => envInt('SERVER_MAX_COROUTINE', 3000),
 ];
 
 if (!empty($pidFile = env('SERVER_PID_FILE'))) {
