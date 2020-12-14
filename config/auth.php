7aa0220c169ca3e6f25fd4755ad2db0603064208
luoxiaojun <luoxiaojun1992@sina.cn>
2020-12-14 18:09:30 +0800

    remove calling old functions

--- config/auth.php
+++ config/auth.php
@@ -1,25 +1,25 @@
 <?php
 
 return [
-    'guard' => env('AUTH_GUARD', 'token'),
+    'guard' => \SwFwLess\components\functions\env('AUTH_GUARD', 'token'),
     'guards' => [
         'token' => [
             'guard' => \SwFwLess\components\auth\token\Guard::class,
             'user_provider' => \SwFwLess\models\TokenUser::class,
-            'credential_key' => env('AUTH_TOKEN_CREDENTIAL_KEY', 'auth_token'),
+            'credential_key' => \SwFwLess\components\functions\env('AUTH_TOKEN_CREDENTIAL_KEY', 'auth_token'),
         ],
         'jwt' => [
             'guard' => \SwFwLess\components\auth\jwt\Guard::class,
             'user_provider' => \SwFwLess\models\JwtUser::class,
-            'credential_key' => env('AUTH_JWT_CREDENTIAL_KEY', 'Authorization'),
-            'sign_key' => env('AUTH_JWT_SIGN_KEY', ''),
-            'jid' => env('AUTH_JWT_JID', ''),
-            'expiration' => envInt('AUTH_JWT_EXPIRATION', 86400),
+            'credential_key' => \SwFwLess\components\functions\env('AUTH_JWT_CREDENTIAL_KEY', 'Authorization'),
+            'sign_key' => \SwFwLess\components\functions\env('AUTH_JWT_SIGN_KEY', ''),
+            'jid' => \SwFwLess\components\functions\env('AUTH_JWT_JID', ''),
+            'expiration' => \SwFwLess\components\functions\envInt('AUTH_JWT_EXPIRATION', 86400),
         ],
         'basic' => [
             'guard' => \SwFwLess\components\auth\basic\Guard::class,
             'user_provider' => \SwFwLess\models\BasicUser::class,
-            'credential_key' => env('AUTH_BASIC_CREDENTIAL_KEY', 'Authorization'),
+            'credential_key' => \SwFwLess\components\functions\env('AUTH_BASIC_CREDENTIAL_KEY', 'Authorization'),
         ],
     ],
 ];
