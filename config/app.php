fbc4bfd87cffd6e48e291384c8926fc9d0210391
luoxiaojun <luoxiaojun1992@sina.cn>
2020-12-14 18:07:31 +0800

    remove calling old functions

--- config/app.php
+++ config/app.php
@@ -3,16 +3,16 @@
 return [
     //Cors
     'cors' => [
-        'origin' => env('CORS_ORIGIN', ''),
-        'switch' => envInt('CORS_SWITCH', 0),
+        'origin' => \SwFwLess\components\functions\env('CORS_ORIGIN', ''),
+        'switch' => \SwFwLess\components\functions\envInt('CORS_SWITCH', 0),
     ],
 
     //Timezone
-    'timezone' => env('TIMEZONE', 'PRC'),
+    'timezone' => \SwFwLess\components\functions\env('TIMEZONE', 'PRC'),
 
     //Monitor
     'monitor' => [
-        'switch' => envInt('MONITOR_SWITCH', 0),
+        'switch' => \SwFwLess\components\functions\envInt('MONITOR_SWITCH', 0),
     ],
 
     //Throttle
@@ -20,29 +20,29 @@ return [
         'metric' => function(\SwFwLess\components\http\Request $request){
             return $request->getRoute();
         },
-        'period' => envInt('THROTTLE_PERIOD', 60),
-        'throttle' => envInt('THROTTLE_THROTTLE', 10000),
+        'period' => \SwFwLess\components\functions\envInt('THROTTLE_PERIOD', 60),
+        'throttle' => \SwFwLess\components\functions\envInt('THROTTLE_THROTTLE', 10000),
     ],
 
     //RedLock
     'red_lock' => [
-        'connection' => env('RED_LOCK_CONNECTION', 'red_lock'),
+        'connection' => \SwFwLess\components\functions\env('RED_LOCK_CONNECTION', 'red_lock'),
     ],
 
     //RateLimit
     'rate_limit' => [
-        'connection' => env('RATE_LIMIT_CONNECTION', 'rate_limit'),
+        'connection' => \SwFwLess\components\functions\env('RATE_LIMIT_CONNECTION', 'rate_limit'),
     ],
 
     //Cache
     'cache' => [
-        'connection' => env('CACHE_CONNECTION', 'cache'), //redis connection
-        'update_lock_ttl' => envInt('CACHE_UPDATE_LOCK_TTL', 10),
+        'connection' => \SwFwLess\components\functions\env('CACHE_CONNECTION', 'cache'), //redis connection
+        'update_lock_ttl' => \SwFwLess\components\functions\envInt('CACHE_UPDATE_LOCK_TTL', 10),
     ],
 
     //Hot Reload
     'hot_reload' => [
-        'switch' => envInt('HOT_RELOAD_SWITCH', 0),
+        'switch' => \SwFwLess\components\functions\envInt('HOT_RELOAD_SWITCH', 0),
         'watch_dirs' => [
             __DIR__ . '/',
             __DIR__ . '/../app/',
@@ -51,7 +51,7 @@ return [
         ],
         'excluded_dirs' => [],
         'watch_suffixes' => ['.php', '.env'],
-        'driver' => env('HOT_RELOAD_DRIVER', \Kwf\FileWatcher\Watcher::class), //HuangYi\Watcher\Watcher::class is another choice
+        'driver' => \SwFwLess\components\functions\env('HOT_RELOAD_DRIVER', \Kwf\FileWatcher\Watcher::class), //HuangYi\Watcher\Watcher::class is another choice
     ],
 
     //Error handler
@@ -63,8 +63,8 @@ return [
 
     //Ip Restriction
     'ip_restriction' => [
-        'ips' => env('IP_RESTRICTION_IPS'),
-        'api_prefix' => env('IP_RESTRICTION_API_PREFIX'),
+        'ips' => \SwFwLess\components\functions\env('IP_RESTRICTION_IPS'),
+        'api_prefix' => \SwFwLess\components\functions\env('IP_RESTRICTION_API_PREFIX'),
     ],
 
     //Scheduler
@@ -84,5 +84,5 @@ return [
     ],
 
     //You can turn off the switch to improve the performance
-    'route_di_switch' => envBool('ROUTE_DI_SWITCH', false),
+    'route_di_switch' => \SwFwLess\components\functions\envBool('ROUTE_DI_SWITCH', false),
 ];
