fdc1eab3213c485b8e056beda9bf195c820b70ee
luoxiaojun <luoxiaojun1992@sina.cn>
2020-12-15 13:56:40 +0800

    remove calling old functions

--- config/log.php
+++ config/log.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'path' => env('LOG_PATH', __DIR__ . '/../runtime/logs/app-{date}.log'),
-    'level' => envInt('LOG_LEVEL', \Monolog\Logger::DEBUG),
-    'pool_size' => envInt('LOG_POOL_SIZE', 100),
-    'buffer_max_size' => envInt('LOG_BUFFER_MAX_SIZE', 10),
-    'name' => env('LOG_NAME', 'sw-fw-less'),
-    'reserve_days' => envInt('LOG_RESERVE_DAYS', 3),
-    'switch' => envInt('LOG_SWITCH', 0),
+    'path' => \SwFwLess\components\functions\env('LOG_PATH', __DIR__ . '/../runtime/logs/app-{date}.log'),
+    'level' => \SwFwLess\components\functions\envInt('LOG_LEVEL', \Monolog\Logger::DEBUG),
+    'pool_size' => \SwFwLess\components\functions\envInt('LOG_POOL_SIZE', 100),
+    'buffer_max_size' => \SwFwLess\components\functions\envInt('LOG_BUFFER_MAX_SIZE', 10),
+    'name' => \SwFwLess\components\functions\env('LOG_NAME', 'sw-fw-less'),
+    'reserve_days' => \SwFwLess\components\functions\envInt('LOG_RESERVE_DAYS', 3),
+    'switch' => \SwFwLess\components\functions\envInt('LOG_SWITCH', 0),
 ];
