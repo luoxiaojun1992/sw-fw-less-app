932d05f6755180cd8d19d76b2c748099c0a13d5c
luoxiaojun <luoxiaojun1992@sina.cn>
2021-03-08 10:34:25 +0800

    feat: ntp

--- config/time.php
+++ config/time.php
@@ -1,1 +1,21 @@
 <?php
+
+return [
+    'ntp' => [
+        'switch' => \SwFwLess\components\functions\envBool('TIME_NTP_SWITCH', false),
+        'servers' => [
+            [
+                'host' => 'time1.aliyun.com',
+                'pool_size' => 1,
+            ],
+            [
+                'host' => 'time2.aliyun.com',
+                'pool_size' => 1,
+            ],
+            [
+                'host' => 'time3.aliyun.com',
+                'pool_size' => 1,
+            ],
+        ],
+    ]
+];
