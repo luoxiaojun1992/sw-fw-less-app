0e7f19c916bf2e1f299b0b3f1456915499be6a9e
luoxiaojun <luoxiaojun1992@sina.cn>
2022-01-24 19:15:55 +0800

    chore: remove useless config

--- config/auth.php
+++ config/auth.php
@@ -3,23 +3,13 @@
 return [
     'guard' => \SwFwLess\components\functions\env('AUTH_GUARD', 'token'),
     'guards' => [
-        'token' => [
-            'guard' => \SwFwLess\components\auth\token\Guard::class,
-            'user_provider' => \SwFwLess\models\TokenUser::class,
-            'credential_key' => \SwFwLess\components\functions\env('AUTH_TOKEN_CREDENTIAL_KEY', 'auth_token'),
-        ],
         'jwt' => [
             'guard' => \SwFwLess\components\auth\jwt\Guard::class,
-            'user_provider' => \SwFwLess\models\JwtUser::class,
+            'user_provider' => \SwFwLess\components\auth\jwt\JwtUser::class,
             'credential_key' => \SwFwLess\components\functions\env('AUTH_JWT_CREDENTIAL_KEY', 'Authorization'),
             'sign_key' => \SwFwLess\components\functions\env('AUTH_JWT_SIGN_KEY', ''),
             'jid' => \SwFwLess\components\functions\env('AUTH_JWT_JID', ''),
             'expiration' => \SwFwLess\components\functions\envInt('AUTH_JWT_EXPIRATION', 86400),
-        ],
-        'basic' => [
-            'guard' => \SwFwLess\components\auth\basic\Guard::class,
-            'user_provider' => \SwFwLess\models\BasicUser::class,
-            'credential_key' => \SwFwLess\components\functions\env('AUTH_BASIC_CREDENTIAL_KEY', 'Authorization'),
         ],
     ],
 ];
